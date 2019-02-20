214312b3e88e31a07e0bef098b520bc7b9fdb11d
Cristopher Jimenez <crisjimenez120@gmail.com>
2019-02-20 11:32:17 -0500

    deploying to heroku

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def home():
 		return render_template('index.html', todos = data, test = sillyauth)
 	else:
 		return render_template('login.html')
-	
+
 @app.route('/register', methods = ['POST', 'GET'])
 def register():
 	if request.method == 'POST':
@@ -76,7 +76,7 @@ def flip_task_false(task):
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host="0.0.0.0", port=port, threaded=True, debug=True)
+	app.run(host="0.0.0.0", port=port, threaded=True)
 
 
 
